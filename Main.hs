ec04c4ccb0c325d114ed1e625fa92865f3c8e3a8
WhiteDragon <whitedragon@classicgraal.net>
2011-02-01 00:47:48 -0500

    Search the subdirectories npcs/, weapons/, scripts/ of the directory passed in as the first parameter. This will probably change soon but it works as debugging for now.

--- Main.hs
+++ Main.hs
@@ -4,18 +4,40 @@ import GDoc
 import GDoc.Renderer.Html
 import Text.Highlighting.Kate
 import Text.XHtml.Strict (showHtml, (+++), Html, body, header, thelink, thetype, src, (!), noHtml, rel, href)
+import System.Directory
+import System.Environment
+import Control.Monad
 
 out :: IO ()
 out = putStrLn . generate =<< readFile "examples/chat_system"
 
 main :: IO ()
 main = do
-  code <- readFile "examples/chat_system"
-  writeFile "gdoc.html" (generate code)
-  writeFile "source.html" $ either (error . show) (showHtml . formatSource) $ highlightAs "javascript" code
+  args <- getArgs
+  when (length args < 0) (fail "Please provide a directory as the first argument.") -- containing weapons/, npcs/, and scripts/ in it.")
+  let dir = head args
+  
+  npcPaths <- fmap cleanPaths $ getDirectoryContents (dir++"/npcs")
+  scriptPaths <- fmap cleanPaths $ getDirectoryContents (dir++"/scripts")
+  weaponPaths <- fmap cleanPaths $ getDirectoryContents (dir++"/weapons")
+  
+  createDirectory "docs"
+  forM_ npcPaths (f dir "npcs")
+  forM_ scriptPaths (f dir "scripts")
+  forM_ weaponPaths (f dir "weapons")
+  
+  where
+    f dir d s = do
+      putStrLn ("Parsing: " ++ d ++ "/" ++ s)
+      code <- readFile (dir ++ "/" ++ d ++ "/" ++ s)
+      writeFile ("docs/" ++ d ++ "_" ++ s ++ ".html") (generate code)
+      writeFile ("docs/" ++ d ++ "_" ++ s ++ "_source.html") $ either (error . show) (showHtml . formatSource) $ highlightAs "javascript" code
 
 formatSource :: [SourceLine] -> Html
 formatSource d = (header $ thelink ! [rel "stylesheet", thetype "text/css", href "screen.css"] $ noHtml) +++ (body . formatAsXHtml [OptNumberLines, OptLineAnchors] "javascript" $ d)
 
+cleanPaths :: [FilePath] -> [FilePath]
+cleanPaths = filter (\s -> s /= "." && s /= "..")
+  
 generate :: String -> String
 generate = renderScript . either (error . show) id . parseDoc
